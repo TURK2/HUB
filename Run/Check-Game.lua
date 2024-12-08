6ec5f82947685291ba2b8a6c9602d46e47338505
TURK2 <maytawin24022553@gmail.com>
2024-12-08 18:55:16 +0700

    up

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,7 +1,7 @@
 getgenv().Games = { -- print(game.PlaceId)
 
-    [2753915549] = "", -- Blox Fruit โลก 1
-    [4442272183] = "", -- Blox Fruit โลก 2
-    [7449423635] = "", -- Blox Fruit โลก 3
-    [16732694052] = "", -- Fisch
+    [2753915549] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 1
+    [4442272183] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 2
+    [7449423635] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 3
+    [16732694052] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/Fisch.lua", -- Fisch
   }
--- Run/Run-Script.lua
+++ Run/Run-Script.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Run/Check-Game.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
