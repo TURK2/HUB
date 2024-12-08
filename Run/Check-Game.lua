3754b8e365676418493d152c0a329f244a23a285
TURK2 <maytawin24022553@gmail.com>
2024-12-08 18:52:36 +0700

    upload

---
+++ Run/Check-Game.lua
@@ -0,0 +1,7 @@
+getgenv().Games = { -- print(game.PlaceId)
+
+    [2753915549] = "", -- Blox Fruit โลก 1
+    [4442272183] = "", -- Blox Fruit โลก 2
+    [7449423635] = "", -- Blox Fruit โลก 3
+    [16732694052] = "", -- Fisch
+  }
