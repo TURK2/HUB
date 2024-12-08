bd1cd519087349fa7b311dba924a4e7a0e00c1b7
TURK2 <maytawin24022553@gmail.com>
2024-12-08 17:47:16 +0700

    up

--- main-hub/TURKKUNHUB.lua
+++ main-hub/TURKKUNHUB.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("...."))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/main-hub/GameList.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
