23251ad8c7457124feb11d8c04a71987ae5da860
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-06-15 02:18:04 +0700

    Update Run-Script.lua

--- Run/Run-Script.lua
+++ Run/Run-Script.lua
@@ -1,7 +1,10 @@
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/TURK2/HUB/main/Run/Check-Game.lua"))()
+print("📦 โหลดตารางเกมสำเร็จ:", Games)
 
 for PlaceID, Execute in pairs(Games) do
+    print("🔍 กำลังตรวจ:", PlaceID, Execute)
     if PlaceID == game.PlaceId then
+        print("✅ ตรงกับ PlaceId! กำลังโหลด:", Execute)
         loadstring(game:HttpGet(Execute))()
     end
 end
