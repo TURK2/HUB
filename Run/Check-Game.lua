d472637a75b259b52b957e084fa00b8fdc3f12f1
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-06-15 01:28:23 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -4,4 +4,5 @@ getgenv().Games = { -- print(game.PlaceId)
     [4442272183] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 2
     [7449423635] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 3
     [16732694052] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/Fisch.lua", -- Fisch
+    [8003084678] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/oMega Obby.lua", -- oMega Obby
   }
