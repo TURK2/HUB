2b00b5f200b1bd2abc87dd3261da741eacbc6ba0
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-06-15 01:47:38 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,8 +1,9 @@
-getgenv().Games = { -- print(game.PlaceId)
+local Games = {
+  [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 3
+  [8003084678] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/oMega%20Obby.lua", -- oMega Obby
 
-    [2753915549] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 1
-    [4442272183] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 2
-    [7449423635] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit โลก 3
-    [16732694052] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/Fisch.lua", -- Fisch
-    [8003084678] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/oMega Obby.lua", -- oMega Obby
-  }
+}
+
+return Games
