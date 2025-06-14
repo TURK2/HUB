79e94449b464238837dc51ebcd47e8dfefc1372f
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-06-15 02:12:07 +0700

    Update Run-Script.lua

--- Run/Run-Script.lua
+++ Run/Run-Script.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.github.com/TURK2/HUB/blob/main/Run/Check-Game.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/TURK2/HUB/main/Run/Check-Game.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
