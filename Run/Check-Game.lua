110c80b4c49124373f2c5bc0f35c214a2d6a8600
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-06-15 01:52:01 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,7 +1,4 @@
 local Games = {
-  [2753915549] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit Sea 1
-  [4442272183] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/BloxFruit.lua", -- Blox Fruit Sea 3
   [8003084678] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/oMega%20Obby.lua", -- oMega Obby
 
 }
