fe71d3a3ed5a4cfb72314b03dff5e627225b0d21
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-08-23 18:11:43 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,6 +1,7 @@
 
 local Games = {
     [8003084678] = "https://raw.githubusercontent.com/TURK2/HUB/main/Run/Run-Script.lua",
+    [97777561575736] = "https://raw.githubusercontent.com/TURK2/HUB/refs/heads/main/Script-Game.lua/Kayak%20Racing", -- Kayak Racing
 }
 
 return Games
