ed904e5fb25d9dfad138a51870b436068ed454cd
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-08-24 02:15:54 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,6 +1,18 @@
--- เก็บ Game PlaceId + ลิ้ง script
-return {
+-- Store Game PlaceId + Script links
+local GameScripts = {
     [97777561575736] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/KayakRacing.lua", -- KayakRacing
-    [662417684] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/LuckyBlock.lua", -- LuckyBlock 
-    -- เพิ่มเรื่อย ๆ ได้   
+    [662417684] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/LuckyBlock.lua", -- LuckyBlock
+    -- Add more here
 }
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local PlaceId = game.PlaceId
+
+if GameScripts[PlaceId] then
+    -- If found, load script
+    loadstring(game:HttpGet(GameScripts[PlaceId]))()
+else
+    -- If not found, kick player
+    LocalPlayer:Kick("เกมนี้ไม่รองรับ ❌\nThis game is not supported ❌")
+end
