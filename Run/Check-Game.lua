c4a55ba41827f395ecb0e4b6c27bdf3d8bf39cfd
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-08-23 18:37:44 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,5 +1,5 @@
 -- เก็บ Game PlaceId + ลิ้ง script
 return {
-    [97777561575736] = "", -- เกมที่ 
+    [97777561575736] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/KayakRacing.lua", -- เกมที่ 
     -- เพิ่มเรื่อย ๆ ได้
 }
