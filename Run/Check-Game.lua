8e3bc0376232c1f0f58df551d7e6f16fed3e3199
TURK2 <137105402+TURK2@users.noreply.github.com>
2025-08-24 13:50:31 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,6 +1,6 @@
 -- เก็บ Game PlaceId + ลิ้ง Script
 local GameScripts = {
-    [97777561575736] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/KayakRacing.lua", -- KayakRacing
+    [97777561575736] = "https://raw.githubusercontent.com/TURK2/HUB/smain/Script-Game.lua/KayakRacing.lua", -- KayakRacing
     [662417684] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/LuckyBlock.lua", -- LuckyBlock
     -- เพิ่มได้เรื่อยๆ
 }
