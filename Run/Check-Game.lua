cf99b69ff30c799d83eec9dab7b856a54c79feff
TURK2 <137105402+TURK2@users.noreply.github.com>
2026-03-06 08:54:37 +0700

    Update Check-Game.lua

--- Run/Check-Game.lua
+++ Run/Check-Game.lua
@@ -1,7 +1,7 @@
 return {
     [97777561575736] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/KayakRacing.lua", -- KayakRacing
     [662417684] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/LuckyBlock.lua", -- LuckyBlock
-    [83403180598996] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/limbtoInfinity.lua" -- Climb to Infinity!
+    [83403180598996] = "https://raw.githubusercontent.com/TURK2/HUB/main/Script-Game.lua/limbtoInfinity.lua" -- ClimbtoInfinity!
     -- เพิ่มได้เรื่อยๆ
 }
 
